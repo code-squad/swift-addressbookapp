eb3600cd8ab9ac6876a30c4a8b67142af908f0f8
ChocOZerO <nayalth@gmail.com>
2018-03-23 12:18:13 +0900

    [Update]UITableViewDataSource 프로토콜 적용 #7

--- AddressBookApp/AddressBookApp/HolidayViewController.swift
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -8,21 +8,34 @@
 
 import UIKit
 
-class HolidayViewController: UIViewController {
+class HolidayViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    private var holidays: [[String: String]]? = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self as? UITableViewDataSource
+        tableView.dataSource = self
         let path = Bundle.main.path(forResource: "Holidays", ofType: "json")
         let url = URL(fileURLWithPath: path!)
-        guard let data = try? Data.init(contentsOf: url) else { return }
-        guard let holidays = try? JSONDecoder().decode([[String:String]].self, from: data) else { return }
-        print(holidays)
-
+        guard let data = try? Data(contentsOf: url) else { return }
+        do {
+            holidays = try JSONSerialization.jsonObject(with: data, options: []) as? [[String : String]]
+        } catch {
+            print("JSON data failed")
+        }
     }
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return holidays?.count ?? 0
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "holiday", for: indexPath)
+        cell.textLabel?.text = holidays?[indexPath.row]["date"]
+        cell.detailTextLabel?.text = holidays?[indexPath.row]["subtitle"]
+        return cell
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
