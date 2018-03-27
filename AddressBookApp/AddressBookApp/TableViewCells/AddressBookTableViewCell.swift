55ae0ff23d215b2c8ba1607869112b1e299a42c8
ChocOZerO <nayalth@gmail.com>
2018-03-27 13:57:49 +0900

    [Add]addressCell #14

---
+++ AddressBookApp/AddressBookApp/TableViewCells/AddressBookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddressBookTableViewCell.swift
+//  AddressBookApp
+//
+//  Created by TaeHyeonLee on 2018. 3. 27..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class AddressBookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
