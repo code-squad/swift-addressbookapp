d05d265d663a3aa392113d23d93c1e0e81468dcc
ChocOZerO <nayalth@gmail.com>
2018-03-27 17:07:52 +0900

    [Update]주소록 정보 셀에 표시 #15

--- AddressBookApp/AddressBookApp/TableViewCells/AddressBookTableViewCell.swift
+++ AddressBookApp/AddressBookApp/TableViewCells/AddressBookTableViewCell.swift
@@ -7,8 +7,45 @@
 //
 
 import UIKit
+import Contacts
 
 class AddressBookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var telLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+
+    func setCell(with contact: CNContact) {
+        setProfile(with: contact.imageData)
+        setName(faimly: contact.familyName, given: contact.givenName)
+        setTel(among: contact.phoneNumbers)
+        setEmail(among: contact.emailAddresses)
+    }
+
+    private func setProfile(with imageData: Data?) {
+        if let imageData = imageData {
+            self.profile.image = UIImage(data: imageData)
+        } else {
+            self.profile.image = UIImage(named: "addressbook-default-profile")
+        }
+    }
+
+    private func setName(faimly: String, given: String) {
+        nameLabel.text = faimly + given
+    }
+
+    private func setTel(among phoneNumbers: [CNLabeledValue<CNPhoneNumber>]) {
+        telLabel.text = phoneNumbers.count > 0
+            ? CNLabeledValue<CNPhoneNumber>.localizedString(forLabel: phoneNumbers.first!.value.stringValue)
+            : ""
+    }
+
+    private func setEmail(among emailAddresses: [CNLabeledValue<NSString>]) {
+        emailLabel.text = emailAddresses.count > 0
+            ? CNLabeledValue<NSString>.localizedString(forLabel: emailAddresses.first!.label!)
+            : ""
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +54,6 @@ class AddressBookTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
 
