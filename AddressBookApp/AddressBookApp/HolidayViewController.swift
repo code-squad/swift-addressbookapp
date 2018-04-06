30c2bd11a4cdc5a9a484a0c7bd7b644dc8f89961
dely2p <lhyal2p@gmail.com>
2018-04-06 16:33:47 +0900

    add HolidayViewController and connect with storyboard #6
    HolidayViewController를 추가하고 스토리보드에 연결함

---
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HolidayViewController.swift
+//  AddressBookApp
+//
+//  Created by Eunjin Kim on 2018. 4. 6..
+//  Copyright © 2018년 Eunjin Kim. All rights reserved.
+//
+
+import UIKit
+
+class HolidayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
