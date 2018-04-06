cd91d7773b5a2d8c06051c570708d4cbd98ddb8a
dely2p <lhyal2p@gmail.com>
2018-04-06 21:12:03 +0900

    add JSONSerialization to make Array<Dictionary<String,String>> data #7
    Array<Dictionary<String,String>>형태의 데이터를 만들기 위해서 JSONSerialization를 사용함

--- AddressBookApp/AddressBookApp/HolidayViewController.swift
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -13,8 +13,9 @@ class HolidayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let urlPath = "/Users/elly/Documents/ios_Level3/swift-addressbookapp/AddressBookApp/AddressBookApp/HolidayJsonData.json"
-        if let contents = try? String(contentsOfFile: urlPath) {
-            print(contents)
+        if let contents = try? String(contentsOfFile: urlPath).data(using: .utf8) {
+            let json = try! JSONSerialization.jsonObject(with: contents!, options: []) as! Array<Dictionary<String,String>>
+            print(json)
         }
     }
 
