32c61650710c3e02dbd34724c2f0ce2df88f25da
dely2p <lhyal2p@gmail.com>
2018-04-15 00:46:55 +0900

    add ContactData to make DTO #25
    주소록 데이터를 가져올 때 사용할 DTO ContactData 생성함

---
+++ AddressBookApp/AddressBookApp/ContactData.swift
@@ -0,0 +1,15 @@
+//
+//  ContactData.swift
+//  AddressBookApp
+//
+//  Created by Eunjin Kim on 2018. 4. 15..
+//  Copyright © 2018년 Eunjin Kim. All rights reserved.
+//
+
+import Foundation
+
+struct ContactData {
+    var name: String
+    var phoneNumber: String
+    var email: String
+}
--- AddressBookApp/AddressBookApp/ContactsDataLoader.swift
+++ AddressBookApp/AddressBookApp/ContactsDataLoader.swift
@@ -11,29 +11,33 @@ import Contacts
 
 class ContactsDataLoader {
     
-    func accessToContacts() {
+    init() {
         let store = MGCContactStore()
-        var contacts = [MGCContact]()
+        var contactsData =  [ContactData]()
         
         store.checkContactsAccess({
             granted in
             // 주소록 접근 권한 허용시
             if granted {
                 store.fetchContacts({
-                    contact in
+                    contacts in
                     for info in contacts {
-                    
+                        let name = info.familyName + info.givenName
+                        guard let phoneNumber = info.phoneNumbers.first?.value.stringValue else {
+                            return
+                        }
+                        guard let email = info.emailAddresses.first?.value as String? else{
+                            return
+                        }
+                        contactsData.append(ContactData.init(name: name, phoneNumber: phoneNumber, email: email))
                     }
-                    
                 })
             }
             // 접근 권한 비허용 시
             else {
-                
+                // alert Notification
             }
             
         })
-        
-        
     }
 }
