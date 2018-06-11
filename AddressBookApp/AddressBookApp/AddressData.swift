a00f68674587f5bb3c40b4ad3fe1c50e50123a7d
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-11 16:49:52 +0900

    Feat: Fetch contacts and sets address table view #2 - Add contact usage description - Fetch contacts and set Data source in AddressBookViewController - Add property observer for AddressTableViewCell - Rename struct: Contact Data -> AddressData - Add MyContactsData class for managing [AddressData]

--- AddressBookApp/AddressBookApp/AddressData.swift
+++ AddressBookApp/AddressBookApp/AddressData.swift
@@ -9,18 +9,34 @@
 import Foundation
 import Contacts
 
-struct ContactData {
-    var givenName: String?
-    var familyName: String?
-    var phoneNumber: String?
-    var emailAddress: String?
-    var profileImage: UInt8?
+struct AddressData {
+    var givenName: String
+    var familyName: String
+    var phoneNumber: String
+    var emailAddress: String
+    var profileImage: Data?
 
     init(_ contact: CNContact) {
         self.givenName = contact.givenName ?? " "
         self.familyName = contact.familyName ?? " "
         self.phoneNumber = contact.phoneNumbers.first?.value.stringValue ?? "no phone"
         self.emailAddress = (contact.emailAddresses.first?.value) as String? ?? "no email"
-        self.profileImage = contact.imageData?.first ?? nil
+        self.profileImage = contact.imageData
+    }
+}
+
+class MyContactsData {
+    private var contacts: [AddressData]
+
+    init(contacts: [AddressData]) {
+        self.contacts = contacts
+    }
+
+    func data(at: Int) -> AddressData {
+        return self.contacts[at]
+    }
+
+    func count() -> Int {
+        return self.contacts.count
     }
 }
--- AddressBookApp/AddressBookApp/AddressTableViewCell.swift
+++ AddressBookApp/AddressBookApp/AddressTableViewCell.swift
@@ -12,7 +12,20 @@ class AddressTableViewCell: UITableViewCell {
     @IBOutlet weak var profile: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var tellLabel: UIView!
+    @IBOutlet weak var tellLabel: UILabel!
+
+    var status: AddressData! {
+        didSet {
+            self.nameLabel.text = status.givenName + status.familyName
+            self.emailLabel.text = status.emailAddress
+            self.tellLabel.text = status.phoneNumber
+            if let imageData = status.profileImage {
+                self.profile.image = UIImage(data: imageData)
+            } else {
+                self.profile.image = UIImage(named: "addressbook-default-profile")
+            }
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
