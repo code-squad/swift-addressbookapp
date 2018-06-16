24f5af1da40ada7848acce685507910bddc9edcf
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-16 17:23:19 +0900

    Feat: Check Initial consonant of AddressData #5, #6, #7 - Specify a logic for check language type of consonant of name in AddressData - Rename variable - Declare static variables

--- AddressBookApp/AddressBookApp/AddressSets.swift
+++ AddressBookApp/AddressBookApp/AddressSets.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class AddressSets {
-    private var sets: [UInt32 : [AddressData]] = [:]
+    private var addressListWithConsonant: [UInt32 : [AddressData]] = [:]
     private var addressList = [AddressData]()
 
     init(_ address: [AddressData]) {
@@ -18,39 +18,37 @@ class AddressSets {
 
     // value가 하나도 없는 key값은 딕셔너리에서 제거
     func arrange() {
-        self.sets = sets.filter{ $0.value.count > 0 }
+        self.addressListWithConsonant = addressListWithConsonant.filter{ $0.value.count > 0 }
     }
 
-    // 연락처끼리 자기가 같은 초성을 가진 이름인지 판별하여 Bool 리턴
-//    func makeDict() {
-//        addressList.reduce([UInt32: [AddressData]](), { x, y in
-//
-//        })
-//    }
 
     func addressDict() -> [UInt32: [AddressData]]{
-        return sets.filter{ $0.value.count > 0 }
+        return addressListWithConsonant.filter{ $0.value.count > 0 }
     }
 
     // MARK: Check Consonant
 
     static let KOR_CODE_RANGE: CountableClosedRange<UInt32> = 0...18
-    static let KOR_INITIAL_CONSONANT_CODES: [UInt32] = Array(KOR_CODE_RANGE)
-    private let ENG_START_CODE: UInt32 = 65
-    private let ENG_END_CODE: UInt32 = 90
-    static let ALL_LETTERS = KOR_INITIAL_CONSONANT_LETTERS + ENG_INITIAL_CONSONANT_LETTERS
+    static let ENG_CODE_RANGE: CountableClosedRange<UInt32> = (AddressSets.ENG_START_CODE - AddressSets.CONVERTER)...(AddressSets.ENG_END_CODE - AddressSets.CONVERTER)
+    static let ALL_CODE_RANGE: CountableClosedRange<UInt32> = 0...(AddressSets.ENG_END_CODE - AddressSets.CONVERTER + 1)
+    static let CONVERTER: UInt32 = 46
 
+    static let KOR_INITIAL_CONSONANT_CODES: [UInt32] = Array(KOR_CODE_RANGE)
+    static let ALL_LETTERS = KOR_INITIAL_CONSONANT_LETTERS + ENG_INITIAL_CONSONANT_LETTERS
     static let KOR_INITIAL_CONSONANT_LETTERS = [
         "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
     ]
     static let ENG_INITIAL_CONSONANT_LETTERS = [
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#"
     ]
+
+    static let ENG_START_CODE: UInt32 = 65
+    static let ENG_END_CODE: UInt32 = 90
 
     func isEnglish(text: String) -> Bool {
         guard let first = text.uppercased().first else { return false }
         guard let scalarValue = UnicodeScalar(String(first))?.value else { return false }
-        return ENG_START_CODE...ENG_END_CODE ~= scalarValue
+        return AddressSets.ENG_START_CODE...AddressSets.ENG_END_CODE ~= scalarValue
     }
 
     func isKorean(text: String) -> Bool {
@@ -80,15 +78,15 @@ class AddressSets {
         let name = address.name
         // 영어일때
         if isEnglish(text: name) {
-            guard let code = matchEng(text: name) else { return nil }
+            guard let code = matchEng(text: name) else { return AddressSets.ALL_CODE_RANGE.last }
             return code
         }
         // 한글일때
         else if isKorean(text: name) {
-            guard let code = matchKor(text: name) else { return nil }
+            guard let code = matchKor(text: name) else { return AddressSets.ALL_CODE_RANGE.last }
             return code
         } else {
-            return nil
+            return AddressSets.ALL_CODE_RANGE.last
         }
     }
 
@@ -96,22 +94,7 @@ class AddressSets {
         self.checkConsonantOf(address: data)
     }
 
-
-
-}
-
-/*
-private func setAsDictionary (_ beverages: [Beverage]) -> [ObjectIdentifier: [Beverage]] {
-    let stockSets = beverages.reduce(into: [ObjectIdentifier: [Beverage]]()) {
-        $0[ObjectIdentifier(type(of: $1)), default:[]].append($1)
+    func sortAddress() {
+        self.addressList.sort() // name 기준으로 sort
     }
-    return stockSets
-}
 
- let letters = "abracadabra"
- let letterCount = letters.reduce(into: [:]) { counts, letter in
- counts[letter, default: 0] += 1
- }
- // letterCount == ["a": 5, "b": 2, "r": 2, "c": 1, "d": 1]
-
-*/
