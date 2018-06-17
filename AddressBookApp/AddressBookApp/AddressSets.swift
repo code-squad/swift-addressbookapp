e9ac47d944fe833df4df1dd52f559a71fcb79805
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-17 19:25:35 +0900

    Feat: Handle and deliver addressList info #4,#5,#6,#7
    - Features for delivering the data and information of ordered address
    list
    - Set Access control for private functions
    - Delete unused codes

--- AddressBookApp/AddressBookApp/AddressSets.swift
+++ AddressBookApp/AddressBookApp/AddressSets.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class AddressSets {
+class AddressSets { 
 
     // MARK: static properties
 
@@ -29,12 +29,18 @@ class AddressSets {
     static let ENG_START_CODE: UInt32 = 65
     static let ENG_END_CODE: UInt32 = 90
 
-    private var addressListWithConsonant: [UInt32 : [AddressData]] = [:]
-    private var addressList = [AddressData]()
+    // MARK: class properties and initializer
 
+    private var addressList = [AddressData]()
+    private var addressListWithConsonant: [UInt32 : [AddressData]] = [:]
+
+    init() {}
 
     init(_ address: [AddressData]) {
         self.addressList = address.sorted()
+        self.addressListWithConsonant = addressList.reduce(into: [UInt32 : [AddressData]]()) {
+            $0[checkConsonantOf(address: $1), default:[]].append($1)
+        }
     }
 
     // MARK: Check Consonant
@@ -42,13 +48,11 @@ class AddressSets {
     // 영어, 한글, 기타 문자인지 판단
     func checkConsonantOf(address: AddressData) -> UInt32 {
         let name = address.name
-        // 영어일때
+
         if isEnglish(text: name) {
             guard let code = matchEng(text: name) else { return AddressSets.ALL_CODE_RANGE.last! }
             return code
-        }
-            // 한글일때
-        else if isKorean(text: name) {
+        } else if isKorean(text: name) {
             guard let code = matchKor(text: name) else { return AddressSets.ALL_CODE_RANGE.last! }
             return code
         } else {
@@ -57,19 +61,19 @@ class AddressSets {
         }
     }
 
-    func isEnglish(text: String) -> Bool {
+    private func isEnglish(text: String) -> Bool {
         guard let first = text.uppercased().first else { return false }
         guard let scalarValue = UnicodeScalar(String(first))?.value else { return false }
         return AddressSets.ENG_START_CODE...AddressSets.ENG_END_CODE ~= scalarValue
     }
 
-    func isKorean(text: String) -> Bool {
+    private func isKorean(text: String) -> Bool {
         guard let scalarValue = matchKor(text: text) else { return false }
         return AddressSets.KOR_CODE_RANGE ~= scalarValue
     }
 
     // 초성의 KOR INITIAL CONSONANT CODES 리턴
-    func matchKor(text: String) -> UInt32? {
+    private func matchKor(text: String) -> UInt32? {
         guard let text = text.first else { return nil }
         let val = UnicodeScalar(String(text))?.value
         guard let value = val else { return nil }
@@ -78,17 +82,41 @@ class AddressSets {
     }
 
     // 초성의 ENG INITIAL CONSONANT CODES 리턴
-    func matchEng(text: String) -> UInt32? {
+    private func matchEng(text: String) -> UInt32? {
         guard let first = text.uppercased().first else { return nil }
         guard let scalarValue = UnicodeScalar(String(first))?.value else { return nil }
         let converted = scalarValue - 46
         return converted
     }
 
-    func setAddressListWithConsonant() {
-        addressListWithConsonant = addressList.reduce(into: [UInt32 : [AddressData]]()) {
-            $0[checkConsonantOf(address: $1), default:[]].append($1)
-        }
+    // MARK: Information for Dictionary
+
+    func countOfConsonant() -> Int {
+        return self.addressListWithConsonant.keys.count
+    }
+
+    func keys() -> [String] {
+        return self.addressListWithConsonant.keys.sorted().map{ codeToLetter(code: $0) }
+    }
+
+    func count(of letter: String) -> Int {
+        let section = letterToCode(letter: letter)
+        let count = self.addressListWithConsonant[section]?.count ?? 0
+        return count
+    }
+
+    func data(section letter: String, row index: Int) -> AddressData {
+        let section = letterToCode(letter: letter)
+        let address = self.addressListWithConsonant[section]![index]
+        return address
+    }
+
+    private func codeToLetter(code: UInt32) -> String {
+        return AddressSets.ALL_LETTERS[Int(code)]
+    }
+
+    private func letterToCode(letter: String) -> UInt32 {
+        return matchEng(text: letter) ?? matchKor(text: letter) ?? AddressSets.ALL_CODE_RANGE.last!
     }
 
 }
