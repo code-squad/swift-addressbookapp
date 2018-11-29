f9372ae322493187da02524922307918e3ff4164
oingbong <oingbong@gmail.com>
2018-11-29 14:36:08 +0900

    Add HolidayVC & Connect storyboard

---
+++ AddressBookApp/AddressBookApp/HolidayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HolidayViewController.swift
+//  AddressBookApp
+//
+//  Created by oingbong on 29/11/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class HolidayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
