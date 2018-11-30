bceb1e54b4b7c8b1530c549e9cc3f38f698a5e3e
oingbong <oingbong@gmail.com>
2018-11-30 14:32:59 +0900

    Add AddressBookViewController

---
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddressBookViewController.swift
+//  AddressBookApp
+//
+//  Created by oingbong on 30/11/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class AddressBookViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
