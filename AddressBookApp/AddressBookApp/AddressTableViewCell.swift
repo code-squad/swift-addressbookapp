84c2959e84a52d74386caf8fee201a60540130b5
oingbong <oingbong@gmail.com>
2018-11-30 15:30:23 +0900

    Add AddressTableViewCell & Connect Label, Image with Custom Cell

---
+++ AddressBookApp/AddressBookApp/AddressTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  AddressTableViewCell.swift
+//  AddressBookApp
+//
+//  Created by oingbong on 30/11/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var telLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
