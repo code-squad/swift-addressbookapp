3e0defe03531120dc16264458489c59f37376fea
oingbong <oingbong@gmail.com>
2018-12-02 17:41:30 +0900

    Add identifier_name in disabled_rules for consonant & Add related consonant func

--- AddressBookApp/AddressBookApp/AddressList.swift
+++ AddressBookApp/AddressBookApp/AddressList.swift
@@ -11,6 +11,10 @@ import UIKit
 class AddressList {
     private var addresses = [Address]()
     
+    enum KoreanConsonant: Int {
+        case ㄱ = 0, ㄲ, ㄴ, ㄷ, ㄸ, ㄹ, ㅁ, ㅂ, ㅃ, ㅅ, ㅆ, ㅇ, ㅈ, ㅉ, ㅊ, ㅋ, ㅌ, ㅍ, ㅎ
+    }
+    
     var count: Int {
         return addresses.count
     }
@@ -30,6 +34,8 @@ class AddressList {
                 let profile = contact.imageData
                 
                 self.addresses.append(Address(name: name, tel: tel, email: email, profile: profile))
+
+                guard let consonant = self.fetchConsonant(from: name) else { continue }
             }
             controller.tableView.reloadData()
         }
@@ -37,6 +43,25 @@ class AddressList {
     
     func append(with address: Address) {
         self.addresses.append(address)
+    }
+
+    private func fetchConsonant(from name: String) -> Character? {
+        /*
+         영어 : 앞글자 리턴 (유니코드 65 - 122)
+         한글 : 앞글자 초성 리턴
+         */
+        guard let text = name.first else { return nil }
+        guard let value = UnicodeScalar(String(text))?.value else { return nil }
+        guard value > 122 else { return text }
+        let index = (value - 0xAC00) / 28 / 21
+        let consonant = extractConsonant(at: index)
+        return consonant
+    }
+    
+    private func extractConsonant(at index: UInt32) -> Character? {
+        guard let unicodeConsonant = UnicodeScalar(0x1100 + index) else { return nil }
+        let consonant = Character(unicodeConsonant)
+        return consonant
     }
     
     subscript(index: Int) -> Address {
