fb1ca0758b28594265b736e5b5a91e7d8ffa474b
oingbong <oingbong@gmail.com>
2018-12-03 16:23:58 +0900

    Delete lazy variable

--- AddressBookApp/AddressBookApp/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Contacts
 
 class AddressBookViewController: UITableViewController {
-    private lazy var addresses = Addresses()
+    private let addresses = Addresses()
     
     override func viewDidLoad() {
         super.viewDidLoad()
