628106447250b83da1b0d34d1072153df941a494
Dongmin.yoon <ehdals8965@naver.com>
2019-04-01 20:42:35 +0900

    Made Custom Cell

--- AddressbookApp/AddressbookApp/AddressBookViewController.swift
+++ AddressbookApp/AddressbookApp/AddressBookViewController.swift
@@ -9,21 +9,37 @@
 import UIKit
 
 class AddressBookViewController: UITableViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        MGCContactStore.sharedInstance.checkContactsAccess { isAccess in
+            if isAccess { print("접속에 성공했습니다.") }
+            else { print("접속에 실패하였습니다.") }
+        }
+        
+        MGCContactStore.sharedInstance.fetchContacts { contacts in
+            
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath) as! AddressTableViewCell
+        cell.profileImageView.image = UIImage(named: "addressbook-default-profile")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
     }
 }
---
+++ AddressbookApp/AddressbookApp/AddressTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddressTableViewCell.swift
+//  AddressbookApp
+//
+//  Created by 윤동민 on 01/04/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var telLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}
