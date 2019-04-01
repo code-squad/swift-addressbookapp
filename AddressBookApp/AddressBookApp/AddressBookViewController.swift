b13872667ae3d24e64baf41120dc8a1d8342c357
hngfu <djsl1018@naver.com>
2019-04-01 22:34:12 +0900

    cell identifier 추가

--- AddressBookApp/AddressBookApp/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddressBookViewController: UITableViewController {
 
+    private let contactCellIdentifier = "contactCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,15 +34,15 @@ class AddressBookViewController: UITableViewController {
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: contactCellIdentifier, for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
