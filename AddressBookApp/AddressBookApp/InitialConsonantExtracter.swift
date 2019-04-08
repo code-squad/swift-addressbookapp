53ce7df8a8850a91aaaf50f7ce00781a8173c2b1
hngfu <djsl1018@naver.com>
2019-04-08 16:50:34 +0900

    한글 초성만 넣은 경우에도 인식하도록 수정

--- AddressBookApp/AddressBookApp/InitialConsonantExtracter.swift
+++ AddressBookApp/AddressBookApp/InitialConsonantExtracter.swift
@@ -13,6 +13,7 @@ struct InitialConsonantExtracter {
     private let englishUpperCaseRange: ClosedRange<UInt32> = 65...90
     private let englishLowerCaseRange: ClosedRange<UInt32> = 97...122
     private let koreanRange: ClosedRange<UInt32> = 0xac00...0xd7a3
+    private let koreanKonsonantRange: ClosedRange<UInt32> = 12593...12622
     
     func extract(word: String) -> String? {
         guard word.count > 0,
@@ -31,6 +32,10 @@ struct InitialConsonantExtracter {
         if koreanRange.contains(value) {
             let consonantIndex = (value - 0xac00) / 28 / 21
             return Unicode.Scalar(consonantIndex + 0x3131)?.description
+        }
+        
+        if koreanKonsonantRange.contains(value) {
+            return Unicode.Scalar(value)?.description
         }
         
         return "#"
