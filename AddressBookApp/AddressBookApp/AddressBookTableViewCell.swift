c64f3bfb9f17f887f36737060e332a29c5ec4188
kiljoonho <kiljh@me.com>
2019-08-21 14:38:27 +0900

    커스텀 셀 구성

---
+++ AddressBookApp/AddressBookApp/AddressBookTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AddressBookTableViewCell.swift
+//  AddressBookApp
+//
+//  Created by joon-ho kil on 8/21/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class AddressBookTableViewCell: UITableViewCell {
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var telLabel: UILabel!
+}
