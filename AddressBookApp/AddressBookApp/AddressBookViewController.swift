2a9685e79ca322fb79b1ca483cf09a6b0aa3ab72
kiljoonho <kiljh@me.com>
2019-08-22 16:31:30 +0900

    처음 실행될 때 한번만 주소록을 조회하도록 수정

--- AddressBookApp/AddressBookApp/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 import Contacts
 
 class AddressBookViewController: UITableViewController {
+    var contactsDTO = [ContactDTO]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        MGCContactStore.sharedInstance.fetchContacts(({(contacts: [CNContact]) in
+            self.contactsDTO = contacts.map({ (contact) -> ContactDTO in
+                return ContactDTO(contact: contact)
+            })
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }))
     }
-
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MGCContactStore.sharedInstance.getContactsCount()
+        
+        return contactsDTO.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath) as! AddressBookTableViewCell
         
-        MGCContactStore.sharedInstance.fetchContacts(({(contacts: [CNContact]) in
-            let contact = contacts[indexPath.row]
-            let contactDTO = ContactDTO(contact: contact)
-            cell.putInfo(contactDTO: contactDTO)
-        }))
+        cell.putInfo(contactDTO: contactsDTO[indexPath.row])
         
         return cell
     }
