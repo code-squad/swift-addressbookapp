616bfe5161b9cc621c114832c3a74baa7963ff6d
kiljoonho <kiljh@me.com>
2019-08-23 21:00:56 +0900

    기존 ContactDTO 배열을 직접 관리하던 것에서 ContactDTOs 객체를 사용하도록 수정

--- AddressBookApp/AddressBookApp/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -10,18 +10,15 @@ import UIKit
 import Contacts
 
 class AddressBookViewController: UITableViewController {
-    var contactsDTO = [ContactDTO]()
+    var contactDTOs = ContactDTOs()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         MGCContactStore.sharedInstance.fetchContacts(({(contacts: [CNContact]) in
-            self.contactsDTO = contacts.map({ (contact) -> ContactDTO in
-                return ContactDTO(contact: contact)
-            })
-            self.contactsDTO.sort(by: { (left, right) -> Bool in
-                return left < right
-            })
+            self.contactDTOs = ContactDTOs(contacts: contacts)
+            self.contactDTOs.sort()
+            
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
@@ -31,13 +28,13 @@ class AddressBookViewController: UITableViewController {
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return contactsDTO.count
+        return contactDTOs.getCount()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell", for: indexPath) as! AddressBookTableViewCell
         
-        cell.putInfo(contactDTO: contactsDTO[indexPath.row])
+        cell.putInfo(contactDTO: contactDTOs.getContactDTO(index: indexPath.row))
         
         return cell
     }
