2424c34389430e79de47402723c1cf690f3fa737
cmindy <cmindy.soft@gmail.com>
2019-11-25 19:29:11 +0900

    feat: 기존 샘플 코드에서 불필요한 부분 수정

--- AddressBookApp/AddressBookApp/Models/MGCContact.swift
+++ AddressBookApp/AddressBookApp/Models/MGCContact.swift
@@ -7,60 +7,36 @@
 */
 
 import UIKit
+import Contacts
 
 class MGCContact {
     // MARK: - Properties
-    
-    /**
-		Maps to the contact type. true indicates that the contact is of type
-		Person. false indicates that its type is Organization.
-	*/
-    var isPerson: Bool
-    
-    /// Maps to the contact's family name.
+
     var lastName: String
-    
-    /// Maps to the contact's given name.
     var firstName: String
-    
-    /// Maps to the contact's associated organization name.
-    var organization: String
-    
-    /// Maps to the contact's phone number.
-    var phoneNumber: MGCLabelValue?
-    
-    /// Maps to the contact's email address
-    var email: MGCLabelValue?
-    
-    /// Maps to the contact's online URL address.
-    var url: MGCLabelValue?
-    
-    /// Stores the contact's entered anniversary date.
-    var anniversary: NSDateComponents?
-    
-    /// Maps to the contact's social profile.
-    var socialProfile: MGCLabelValue?
-    
-    /// Maps to the contact's instant message address.
-    var instantMessage: MGCLabelValue?
-    
-    /// Maps to the contact's postal address.
-    var postalAddress: MGCLabelValue?
-    
-    /// Maps to the contact's thumbnail profile picture.
-    var profilePicture: MGCLabelValue?
-    
+    var phoneNumber: String?
+    var email: String?
+    var profilePictureData: Data?
+
     
     // MARK: - Initialization
     
-    init(isPerson: Bool = true, lastName: String = String(), firstName: String = String(), organization: String = String(), url: MGCLabelValue? = nil, socialProfile: MGCLabelValue? = nil, instantMessage: MGCLabelValue? = nil, anniversary: NSDateComponents? = nil) {
-		self.isPerson = isPerson
-        self.lastName = lastName
-        self.firstName = firstName
-        self.organization = organization
-        self.url = url
-        self.socialProfile = socialProfile
-        self.instantMessage = instantMessage
-        self.anniversary = anniversary
+    init(contact: CNContact) {
+        self.lastName = contact.familyName
+        self.firstName = contact.givenName
+        
+        if let phoneNumberValue = contact.phoneNumbers.first?.value {
+            let phoneNumberText = phoneNumberValue.stringValue
+            self.phoneNumber = phoneNumberText
+        }
+        
+        if let emailValue = contact.emailAddresses.first?.value {
+            let emailText = emailValue as String
+            self.email = emailText
+        }
+        
+        if let imageData = contact.thumbnailImageData {
+            self.profilePictureData = imageData
+        }
     }
 }
