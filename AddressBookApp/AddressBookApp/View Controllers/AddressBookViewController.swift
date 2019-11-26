9f7b70006382934ca65086881071d78110342c7d
cmindy <cmindy.soft@gmail.com>
2019-11-26 16:20:38 +0900

    fix: 데이터 소스 객체를 사용해 테이블뷰를 표시하도록 수정

--- AddressBookApp/AddressBookApp/View Controllers/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/View Controllers/AddressBookViewController.swift
@@ -6,44 +6,24 @@
 //  Copyright © 2019 cmindy. All rights reserved.
 //
 
-import Contacts
 import UIKit
 
 class AddressBookViewController: UITableViewController {
 
     // MARK: - Vars
     
-    var contacts: [Contact] = [] {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    private var dataSource = ContactDataSource()
     
-    // MARK: - Life Cycles
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        MGCContactStore.sharedInstance.fetchContacts { contacts in
-            self.contacts = contacts.map { Contact(contact: $0) }
-        }
-    }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: AddressCell.reuseID, for: indexPath) as? AddressCell else {
-            return .init()
-        }
+        tableView.dataSource = dataSource
         
-        let contact = contacts[indexPath.row]
-        cell.configure(contact)
-        
-        return cell
+        dataSource.contactsDidFetched = { [weak self] in
+            guard let self = self else { return }
+            self.tableView.reloadData()
+        }
     }
 }
