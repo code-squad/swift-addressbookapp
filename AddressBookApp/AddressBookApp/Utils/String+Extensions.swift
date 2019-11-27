165e192b03687fc5295d41a8f86c06b78a09a8de
cmindy <cmindy.soft@gmail.com>
2019-11-27 15:40:56 +0900

    refactor: 사용하지 않는 주석 삭제

--- AddressBookApp/AddressBookApp/Utils/String+Extensions.swift
+++ AddressBookApp/AddressBookApp/Utils/String+Extensions.swift
@@ -13,8 +13,6 @@ extension String {
         guard let firstWord = self.first else { return nil }
         let wordWithKCForm = String(firstWord).precomposedStringWithCompatibilityMapping
         guard let consonant = wordWithKCForm.decomposedStringWithCanonicalMapping.unicodeScalars.first else { return nil }
-        // ㅈㄴㄱㄷ 와 같이 저장된 경우의 처리를 위애 precompose 후 decompose
-        // ㅈ -> \u{3148} -> \u{110C}
         return UnicodeScalar(consonant).description
     }
 }
